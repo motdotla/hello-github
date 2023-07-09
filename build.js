bb11e2d3acb5e4978865c2fad9f71da1ac71da8e
Scott Motte <mot@mot.la>
2023-07-08 23:51:51 -0700

    Just show hello

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 // build.js
 require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it is working
+//console.log(process.env) // remove this after you've confirmed it is working
 
 console.log(`Hello ${process.env.HELLO}`)
